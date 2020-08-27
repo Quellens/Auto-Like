17e45af266c3c4064db8e346d5b852fdca1e8c06
Quellens <joelseckershady@gmail.com>
2020-08-27 13:29:23 +0200

    Liking suceeded! Adding basic script.

--- content.js
+++ content.js
@@ -1,31 +1,23 @@
-//if(window.location.host == "www.youtube.com" && window.location.href.contains("watch")) console.log("Hello Yt");
-console.log("works..");
-
-async function like() {
-  console.log("Hello Yt");
-  const likebutton = await document.querySelector(".style-scope .ytd-toggle-button-renderer");
-  if (likebutton && window.location.host == "www.youtube.com" && window.location.href.includes("watch") && !document.querySelector(".style-scope .yt-icon-button").getAttribute("aria-pressed")) {
-    const { activeElement } = document;
-    likebutton.click();
-    activeElement.focus();
-  }
+function likeDefaultfn(){
+const likeDefault = new MaterialLiker({
+  like_when : "timed",
+  disabled: false,
+})
+likeDefault.init();
 }
 
-//like();
 
-let compare = window.location.href;
-setInterval( async function(){
- let href = window.location.href;
-  if(compare != href){
-    compare = href;
-   setTimeout(()=>{like()},1000)
-  }
 
-},1000)
+let old_url = '';
+let mutationObserver = new MutationObserver( mutations => {
+    mutations.forEach( (_) => {
+            if (location.href != old_url) {
+                old_url = location.href;
+                console.log('URL was changed');
+                setTimeout(()=>{likeDefaultfn()},1000);
+            }
+    });
+});
 
-/*
-new MutationObserver(like).observe(document.querySelector(".style-scope .ytd-toggle-button-renderer"), {
-    childList: true,
-    subtree: true,
-  });
-*/
+mutationObserver.observe(document.documentElement, {childList: true, subtree: true});
+
---
+++ like.js
@@ -0,0 +1,126 @@
+console.log("works..");
+
+let cache = {
+    likeButton : null,
+    dislikeButton: null
+}
+
+class MaterialLiker {
+    constructor(options) {
+      this.options = options;
+      this.init = this.init.bind(this);
+      this.reset = this.reset.bind(this);
+      this.attemptLike = this.attemptLike.bind(this);
+    }
+
+    stop() {
+      if (typeof this.onStop === 'function') {
+        console.log('stopped');
+        this.onStop();
+      }
+    }
+
+    reset() {
+      cache = {};
+    }
+
+    waitForButtons(callback) {
+        let likebutton = document.querySelector('ytd-video-primary-info-renderer g path[d="M1 21h4V9H1v12zm22-11c0-1.1-.9-2-2-2h-6.31l.95-4.57.03-.32c0-.41-.17-.79-.44-1.06L14.17 1 7.59 7.59C7.22 7.95 7 8.45 7 9v10c0 1.1.9 2 2 2h9c.83 0 1.54-.5 1.84-1.22l3.02-7.05c.09-.23.14-.47.14-.73v-1.91l-.01-.01L23 10z"]');
+        let dislikeButton = document.querySelector( 'ytd-video-primary-info-renderer g path[d="M15 3H6c-.83 0-1.54.5-1.84 1.22l-3.02 7.05c-.09.23-.14.47-.14.73v1.91l.01.01L1 14c0 1.1.9 2 2 2h6.31l-.95 4.57-.03.32c0 .41.17.79.44 1.06L9.83 23l6.59-6.59c.36-.36.58-.86.58-1.41V5c0-1.1-.9-2-2-2zm4 0v12h4V3h-4z"]');
+        
+          if (likebutton && dislikeButton) {
+              // Find and store closest buttons
+              cache.likeButton = likebutton.closest('yt-icon-button');
+              cache.dislikeButton = dislikeButton.closest('yt-icon-button');
+        console.log('...buttons ready');
+        callback();
+          }
+      else {
+        setTimeout(() => this.waitForButtons(callback), 1000);
+      }
+    }
+
+    waitForVideo(callback) {
+      console.log('waiting for video...');
+  
+      this.video = document.querySelector('.video-stream');
+      // Does the video exist?
+      if (this.video) {
+        console.log('...video ready');
+        callback();
+          }
+      else {
+        setTimeout(() => this.waitForVideo(callback), 1000);
+      }
+    }
+  
+    isVideoRated() {
+      return (
+        (
+          cache.likeButton.classList.contains('style-default-active') &&
+          !cache.likeButton.classList.contains('size-default')
+        ) ||
+        cache.dislikeButton.classList.contains('style-default-active')
+      );
+    }
+  
+    attemptLike() {
+  
+      this.waitForButtons(() => {
+        if (this.isVideoRated()) {
+          console.log('video already rated');
+          return this.stop();
+        }
+  
+        cache.likeButton.click();
+        console.log('like button clicked');
+        this.stop();
+      });
+    }
+  
+    init() {
+      console.log('liker initialized');
+      if (this.options.disabled || !document.querySelector('ytd-app[is-watch-page]')) {
+        console.log('liker is disabled or not a watch page');
+        return this.stop();
+      }
+  
+      this.reset();
+  
+      switch (this.options.like_when) {
+
+        case 'timed':
+          return this.waitForVideo(() => {
+              console.log("timer starts")
+            const { video } = this;
+            const onVideoTimeUpdate = e => {
+              // Are we 5 seconds in or at the end of the video?
+              if (video.currentTime >= 5|| video.currentTime >= video.duration) {
+                this.attemptLike();
+                video.removeEventListener('timeupdate', onVideoTimeUpdate);
+              }
+            }
+            video.addEventListener('timeupdate', onVideoTimeUpdate);
+          });
+         break;
+
+        case 'percent':
+          return this.waitForVideo(() => {
+            const { video } = this;
+            const onVideoTimeUpdate = e => {
+              // Are we more than 50% through the video?
+              if (video.currentTime / video.duration >= 0.5) {
+                this.attemptLike();
+                video.removeEventListener('timeupdate', onVideoTimeUpdate);
+              }
+            }
+            video.addEventListener('timeupdate', onVideoTimeUpdate);
+          });
+         break;
+
+        case 'instantly':
+          return this.attemptLike();
+          break;
+      }
+    }
+  }
